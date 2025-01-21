acdd9bce3fdd115a60fe15bf3b3568c36919cbdc
SammLopes <sam.v.p.lopes@gmail.com>
2025-01-20 22:12:06 -0300

    Added seeders RolesTableSeeder.php

---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create([
+            'name' => 'ADM',
+            'slug' => 'admin',
+        ]);
+
+        Role::create([
+            'name' => 'SUP',
+            'slug' => 'user',
+        ]);
+    }
+}
