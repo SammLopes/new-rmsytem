1edbada9a0e278d4ef24d4801b56eed0f7482600
SammLopes <sam.v.p.lopes@gmail.com>
2025-01-20 21:05:27 -0300

    Primeiro Commit adicionando arquivos da pasta de teste

---
+++ resources/lang/pt_BR/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+    'confirmed' => 'Os valores do campo :attribute não coincidem.',
+    'min' => [
+        'string' => 'O campo :attribute deve ter no mínimo :min caracteres.',
+    ],
+    'required' => 'O campo :attribute é obrigatório.',
+    'attributes' => [
+        'password' => 'senha',
+        'password_confirmation' => 'confirmação da senha',
+        'email' => 'e-mail',
+    ],
+];
