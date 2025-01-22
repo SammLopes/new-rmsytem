79653677d8156bf12521fda0c1bad4956223580c
SammLopes <sam.v.p.lopes@gmail.com>
2025-01-21 22:13:46 -0300

    UPdate pagina de visitas e validation.php

--- resources/lang/pt_BR/validation.php
+++ resources/lang/pt_BR/validation.php
@@ -7,9 +7,12 @@ return [
         'string' => 'O campo :attribute deve ter no mínimo :min caracteres.',
     ],
     'required' => 'O campo :attribute é obrigatório.',
+    'unique'=> 'Este :attribute já está cadastrado. ',
     'attributes' => [
         'password' => 'senha',
         'password_confirmation' => 'confirmação da senha',
         'email' => 'e-mail',
+        'cpf' => 'CPF',
+        'cnpj' => 'CNPJ',
     ],
 ];
